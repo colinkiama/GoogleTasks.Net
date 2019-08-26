2f0603d08a9af397648164a61f66144e3dd0088c
Colin Kiama <colinkiama@gmail.com>
2019-08-26 17:53:26 +0100

    Creating classes for use with API

---
+++ GoogleTasksNET/GTask.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleTasksNET
+{
+    public class GTask
+    {
+        [JsonProperty("kind")]
+        public string kind { get; set; }
+
+        [JsonProperty("id")]
+        public string id { get; set; }
+
+        [JsonProperty("etag")]
+        public string etag { get; set; }
+
+        [JsonProperty("title")]
+        public string title { get; set; }
+
+        [JsonProperty("updated")]
+        public DateTime updated { get; set; }
+
+        [JsonProperty("selfLink")]
+        public string selfLink { get; set; }
+
+        [JsonProperty("parent")]
+        public string parent { get; set; }
+
+        [JsonProperty("position")]
+        public string position { get; set; }
+
+        [JsonProperty("notes")]
+        public string notes { get; set; }
+
+        [JsonProperty("status")]
+        public string status { get; set; }
+
+        [JsonProperty("due")]
+        public DateTime due { get; set; }
+
+        [JsonProperty("completed")]
+        public DateTime completed { get; set; }
+
+        [JsonProperty("deleted")]
+        public bool deleted { get; set; }
+
+        [JsonProperty("hidden")]
+        public bool hidden { get; set; }
+
+        [JsonProperty("links")]
+        public List<Link> links { get; set; }
+    }
+
+    public class Link
+    {
+        [JsonProperty("type")]
+        public string type { get; set; }
+
+        [JsonProperty("description")]
+        public string description { get; set; }
+
+        [JsonProperty("link")]
+        public string link { get; set; }
+    }
+
+}
---
+++ GoogleTasksNET/GTaskList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleTasksNET
+{
+    public class GTaskList
+    {
+        public string kind { get; set; }
+        public string id { get; set; }
+        public string etag { get; set; }
+        public string title { get; set; }
+        public DateTime updated { get; set; }
+        public string selfLink { get; set; }
+    }
+}
