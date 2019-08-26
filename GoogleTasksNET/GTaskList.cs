feb73ef971449dd8b6da44b20074100693deb606
Colin Kiama <colinkiama@gmail.com>
2019-08-26 17:54:56 +0100

    Done adding json property atrributes to API classes

--- GoogleTasksNET/GTaskList.cs
+++ GoogleTasksNET/GTaskList.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,11 +7,22 @@ namespace GoogleTasksNET
 {
     public class GTaskList
     {
+        [JsonProperty("kind")]
         public string kind { get; set; }
+
+        [JsonProperty("id")]
         public string id { get; set; }
+
+        [JsonProperty("etag")]
         public string etag { get; set; }
+
+        [JsonProperty("title")]
         public string title { get; set; }
+
+        [JsonProperty("updated")]
         public DateTime updated { get; set; }
+
+        [JsonProperty("selfLink")]
         public string selfLink { get; set; }
     }
 }
