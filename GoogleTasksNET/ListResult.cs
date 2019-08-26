4fe1d4941158624e40380ab4ad69d62a0b3053ef
Colin Kiama <colinkiama@gmail.com>
2019-08-26 18:49:51 +0100

    Created method for getting tasklists

---
+++ GoogleTasksNET/ListResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleTasksNET
+{
+    public class ListResult<T>
+    {
+        public string kind { get; set; }
+
+        public string etag { get; set; }
+
+        public string nextPageToken { get; set; }
+
+        public T[] items { get; set; }
+    }
+}
--- GoogleTasksNET/TasksClient.cs
+++ GoogleTasksNET/TasksClient.cs
@@ -1,11 +1,47 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GoogleTasksNET
 {
     public class TasksClient
     {
+        public string ClientID { get; set; }
+        public string ClientSecret { get; set; }
+        public Token ClientToken { get; set; }
 
+        HttpClient _client = new HttpClient();
+
+        public TasksClient(string clientID, string clientSecret, Token clientToken)
+        {
+            ClientID = clientID;
+            ClientSecret = clientSecret;
+            ClientToken = clientToken;
+        }
+
+
+        public async Task<ListResult<GTaskList>> GetTaskLists(string pageToken = "", ulong maxResults = 20)
+        {
+            ListResult<GTaskList> result = null;
+
+            string requestString = $"https://www.googleapis.com/tasks/v1/users/@me/lists" +
+                $"?maxResults={maxResults}&pageToken={pageToken}";
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestString);
+            request.Headers.Add("Authorization", ClientToken.AccessToken);
+
+            var responseMessage = await _client.SendAsync(request);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string jsonReturned = await responseMessage.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<ListResult<GTaskList>>(jsonReturned);
+            }
+            
+            return result;
+        }
     }
 }
